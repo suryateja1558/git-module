6d1e16a89a389c9a3ffcfb0025afac2eae95daa1
Ubuntu <ubuntu@ip-172-31-87-158.ec2.internal>
2023-08-18 04:12:42 +0000

    terraform proj

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "instance" {
+  ami           = "ami-053b0d53c279acc90" # us-east-1
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
