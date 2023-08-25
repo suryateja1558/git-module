533e3cdd5544e84b33e7bdd1c7a9ba1e073adb11
suryateja1558 <136622853+suryateja1558@users.noreply.github.com>
2023-08-25 09:45:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "instance" {
   ami           = "ami-053b0d53c279acc90" # us-east-1
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "Test-server"
   }
 }
